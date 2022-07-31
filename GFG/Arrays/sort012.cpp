96ee8c7775d08108d1a9e998224c509f11d3ea88
melvinjariwala <melvinjariwala7.mj@gmail.com>
2022-07-31 21:38:07 +0530

    Sort an array of 0s, 1s and 2s

--- GFG/Arrays/sort012.cpp
+++ GFG/Arrays/sort012.cpp
@@ -12,10 +12,8 @@ class Solution
     {
         // code here 
 		int low=0, mid=0, high=n-1;
-		while (mid<=high)
-		{
-			switch (a[mid])
-			{
+		while (mid<=high){
+			switch (a[mid]){
 			case 0:
 				swap(a[low++], a[mid++]);
 				break;
@@ -30,10 +28,7 @@ class Solution
 				break;
 			}
 		}
-		
-
     }
-    
 };
 
 //{ Driver Code Starts.
